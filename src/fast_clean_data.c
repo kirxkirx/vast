a04202fd53557840d3b3e26aae2e19643ec36c67
Kirill Sokolovsky <kirx007@gmail.com>
2019-07-31 18:09:15 +0000

    fast clean for all image*cat* files

--- src/fast_clean_data.c
+++ src/fast_clean_data.c
@@ -18,16 +18,27 @@ int main() {
  if ( dp != NULL ) {
   //while( ep=readdir(dp) ){
   while ( ( ep= readdir( dp ) ) != NULL ) {
-   if ( strlen( ep->d_name ) < 12 )
+   if ( strlen( ep->d_name ) < 12 ) {
     continue; // make sure the filename is not too short for the following tests
+   }
+   // out01234.dat
    if ( ep->d_name[0] == 'o' && ep->d_name[1] == 'u' && ep->d_name[2] == 't' && ep->d_name[strlen( ep->d_name ) - 1] == 't' && ep->d_name[strlen( ep->d_name ) - 2] == 'a' && ep->d_name[strlen( ep->d_name ) - 3] == 'd' ) {
     unlink( ep->d_name );
     continue;
    }
+   // image01234.cat
    if ( ep->d_name[0] == 'i' && ep->d_name[1] == 'm' && ep->d_name[2] == 'a' && ep->d_name[3] == 'g' && ep->d_name[4] == 'e' && ep->d_name[strlen( ep->d_name ) - 1] == 't' && ep->d_name[strlen( ep->d_name ) - 2] == 'a' && ep->d_name[strlen( ep->d_name ) - 3] == 'c' ) {
     unlink( ep->d_name );
     continue;
    }
+   // image*.cat.*
+   if ( ep->d_name[0] == 'i' && ep->d_name[1] == 'm' && ep->d_name[2] == 'a' && ep->d_name[3] == 'g' && ep->d_name[4] == 'e' ) {
+    if( NULL != strstr( ep->d_name, ".cat.") ) {
+     unlink( ep->d_name );
+     continue;
+    }
+   }
+/*
    if ( ep->d_name[0] == 'i' && ep->d_name[1] == 'm' && ep->d_name[2] == 'a' && ep->d_name[3] == 'g' && ep->d_name[4] == 'e' && ep->d_name[strlen( ep->d_name ) - 1] == 'e' && ep->d_name[strlen( ep->d_name ) - 2] == 'r' && ep->d_name[strlen( ep->d_name ) - 3] == 'u' && ep->d_name[strlen( ep->d_name ) - 4] == 't' && ep->d_name[strlen( ep->d_name ) - 5] == 'r' && ep->d_name[strlen( ep->d_name ) - 6] == 'e' && ep->d_name[strlen( ep->d_name ) - 7] == 'p' && ep->d_name[strlen( ep->d_name ) - 8] == 'a' ) {
     unlink( ep->d_name );
     continue;
@@ -39,6 +50,7 @@ int main() {
    }
    // image00055.cat.mag*
    // TBA
+*/
   }
   (void)closedir( dp );
  } else {
