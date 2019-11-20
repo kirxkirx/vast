58b36e8ab1e73aed17800f6499e161a50dbc47f8
Kirill Sokolovsky <kirx007@gmail.com>
2019-11-20 08:33:31 +0000

    fixed candidate links

--- util/transients/make_report_in_HTML.sh
+++ util/transients/make_report_in_HTML.sh
@@ -22,10 +22,11 @@ while read LIGHTCURVE_FILE_OUTDAT B C D E REFERENCE_IMAGE G H ;do
  TRANSIENT_NAME="$TRANSIENT_NAME"_`basename $C`
  echo "Preparing report for the candidate $TRANSIENT_NAME"
  #echo "<h3>$TRANSIENT_NAME</h3>" >> transient_report/index.tmp
- echo "<script>printCandidateNameWithAbsLink('$TRANSIENT_NAME');</script>" >> transient_report/index.tmp
+ echo "<script>printCandidateNameWithAbsLink('$TRANSIENT_NAME');</script>
+ <a href='#$TRANSIENT_NAME'></a>" >> transient_report/index.tmp
  # plot reference image
  util/make_finding_chart $REFERENCE_IMAGE $G $H &>/dev/null && mv pgplot.png transient_report/"$TRANSIENT_NAME"_reference.png
- echo "<img src=\""$TRANSIENT_NAME"_reference.png\"></img>" >> transient_report/index.tmp
+ echo "<img src=\""$TRANSIENT_NAME"_reference.png\">" >> transient_report/index.tmp
  # plot reference image preview
  command -v convert &> /dev/null
  if [ $? -eq 0 ];then
@@ -52,7 +53,7 @@ while read LIGHTCURVE_FILE_OUTDAT B C D E REFERENCE_IMAGE G H ;do
    #echo "Discovery image $N: $DATE  $IMAGE  $X $Y (pix)" >> tmp.description
    # convert -density 45 pgplot.ps pgplot.png
    util/make_finding_chart $IMAGE $X $Y &>/dev/null && mv pgplot.png transient_report/"$TRANSIENT_NAME"_discovery"$N".png
-   echo "<img src=\""$TRANSIENT_NAME"_discovery"$N".png\"></img>" >> transient_report/index.tmp
+   echo "<img src=\""$TRANSIENT_NAME"_discovery"$N".png\">" >> transient_report/index.tmp
   fi
  done < $LIGHTCURVE_FILE_OUTDAT
  
@@ -73,14 +74,14 @@ while read LIGHTCURVE_FILE_OUTDAT B C D E REFERENCE_IMAGE G H ;do
   command -v convert &> /dev/null
   if [ $? -eq 0 ];then
    echo "<a href=\"javascript:toggleElement('fullframepreview_$TRANSIENT_NAME')\">Preview of the reference image(s) and two 2nd epoch images</a></br>" >> transient_report/index.tmp  
-   echo "<div id=\"fullframepreview_$TRANSIENT_NAME\" style=\"display:none\"><img src=\"$REFERENCE_IMAGE_PREVIEW\"></img>" >> transient_report/index.tmp
+   echo "<div id=\"fullframepreview_$TRANSIENT_NAME\" style=\"display:none\"><img src=\"$REFERENCE_IMAGE_PREVIEW\">" >> transient_report/index.tmp
    while read JD MAG ERR X Y APP IMAGE REST ;do
     if [ "$IMAGE" != "$REFERENCE_IMAGE" ];then
      PREVIEW_IMAGE=`basename $IMAGE`_preview.png
      if [ ! -f transient_report/$PREVIEW_IMAGE ];then
       convert $IMAGE -brightness-contrast 30x30 -resize 10% transient_report/$PREVIEW_IMAGE &
      fi
-     echo "<img src=\"$PREVIEW_IMAGE\"></img>" >> transient_report/index.tmp
+     echo "<img src=\"$PREVIEW_IMAGE\">" >> transient_report/index.tmp
     fi
    done < $LIGHTCURVE_FILE_OUTDAT
    wait # just to speed-up the convert thing a bit
