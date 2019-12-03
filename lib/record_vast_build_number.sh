1e6b79f7c1af4d8f9d59339c9b5e6247ffcb8c88
Kirill Sokolovsky <kirx007@gmail.com>
2019-12-03 22:35:02 +0000

    fix the build number script

--- lib/record_vast_build_number.sh
+++ lib/record_vast_build_number.sh
@@ -9,3 +9,6 @@ fi
 # if yes, save the build number to file
 git describe --tags > .cc.build
 
+# exit with success regardless of the result
+# if the source tree was not cloned with git, the build number will not be available
+exit 0
