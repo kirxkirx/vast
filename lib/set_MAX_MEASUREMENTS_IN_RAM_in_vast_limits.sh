4d292997d858bfe90a064d07278f0cf1f7b84d31
Kirill Sokolovsky <kirx007@gmail.com>
2020-03-07 03:43:27 +0000

    MAX_MEASUREMENTS_IN_RAM definition moved to vast_max_measurements_in_ram.h

--- lib/set_MAX_MEASUREMENTS_IN_RAM_in_vast_limits.sh
+++ lib/set_MAX_MEASUREMENTS_IN_RAM_in_vast_limits.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# This script will try to set a safe value of MAX_MEASUREMENTS_IN_RAM in src/vast_limits.h
+#exit 0
+
+# This script will try to set a safe value of MAX_MEASUREMENTS_IN_RAM in src/vast_max_measurements_in_ram.h
 # before compiling VaST
 
 # default conservative values
@@ -46,29 +48,42 @@ fi
 
 # report the result
 echo "This system seems to have $PHYSMEM_BYTES bytes of physical memory 
-Setting MAX_MEASUREMENTS_IN_RAM=$MAX_MEASUREMENTS_IN_RAM in src/vast_limits.h"
+Setting MAX_MEASUREMENTS_IN_RAM=$MAX_MEASUREMENTS_IN_RAM in src/vast_max_measurements_in_ram.h"
 
-if [ ! -f src/vast_limits.h ];then
- echo "ERROR in $0 cannot find file src/vast_limits.h"
- exit 1
+if [ ! -s src/vast_max_measurements_in_ram.h ];then
+ # If there is no such file - write a default one 
+ echo "
+// The following line is just to make sure this file is not included twice in the code
+#ifndef VAST_MAX_MEASUREMENTS_IN_RAM_INCLUDE_FILE
+
+
+#define MAX_MEASUREMENTS_IN_RAM 96000
+
+
+// The macro below will tell the pre-processor that this header file is already included
+#define VAST_MAX_MEASUREMENTS_IN_RAM_INCLUDE_FILE
+
+#endif
+// VAST_MAX_MEASUREMENTS_IN_RAM_INCLUDE_FILE
+" > src/vast_max_measurements_in_ram.h
 fi
 
-N=`grep -c '#define MAX_MEASUREMENTS_IN_RAM' src/vast_limits.h`
+N=`grep -c '#define MAX_MEASUREMENTS_IN_RAM' src/vast_max_measurements_in_ram.h`
 
 if [ $N -ne 1 ];then
- echo "ERROR in $0 '#define MAX_MEASUREMENTS_IN_RAM' appears in src/vast_limits.h more than one time!"
- grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_limits.h
+ echo "ERROR in $0 '#define MAX_MEASUREMENTS_IN_RAM' appears in src/vast_max_measurements_in_ram.h more than one time!"
+ grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_max_measurements_in_ram.h
  exit 1
 fi
 
-LINE_TO_REPLACE=`grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_limits.h`
+LINE_TO_REPLACE=`grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_max_measurements_in_ram.h`
 REPLACE_WITH="#define MAX_MEASUREMENTS_IN_RAM $MAX_MEASUREMENTS_IN_RAM  // set automatically at compile time based on PHYSMEM_BYTES=$PHYSMEM_BYTES by $0"
 
-cat src/vast_limits.h | sed "s:$LINE_TO_REPLACE:$REPLACE_WITH:g" > src/vast_limits.tmp
-mv src/vast_limits.tmp src/vast_limits.h
+cat src/vast_max_measurements_in_ram.h | sed "s:$LINE_TO_REPLACE:$REPLACE_WITH:g" > src/vast_max_measurements_in_ram.tmp
+mv src/vast_max_measurements_in_ram.tmp src/vast_max_measurements_in_ram.h
 
 # Check the replacement
-grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_limits.h
+grep '#define MAX_MEASUREMENTS_IN_RAM' src/vast_max_measurements_in_ram.h
 if [ $? -ne 0 ];then
  echo "OHOH, somehting went wrong in $0"
  exit 1
