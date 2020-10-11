f82c03910f8049bc41c2035b4908490e3dfd7e25
kirxkirx <kirx@kirx.net>
2020-10-11 02:58:19 -0700

    find X11 library on OS X with macports

--- lib/find_x11lib_include.sh
+++ lib/find_x11lib_include.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash                                              
 ########### Try to guess a non-standard X11 libray path ###########
 # Test other possible library paths
-for POSSIBLE_LIBRARY_PATH in /opt/X11/lib /usr/X11R6/lib /usr/lib/X11 /usr/local/lib/X11 /usr/lib64/X11 /usr/local/lib64/X11 /usr/lib32/X11 /usr/local/lib32/X11 ;do
+for POSSIBLE_LIBRARY_PATH in /opt/X11/lib /usr/X11R6/lib /usr/lib/X11 /usr/local/lib/X11 /usr/lib64/X11 /usr/local/lib64/X11 /usr/lib32/X11 /usr/local/lib32/X11 /opt/local/lib ;do
  if [ -d "$POSSIBLE_LIBRARY_PATH" ];then
   LX11="$LX11 -L$POSSIBLE_LIBRARY_PATH"
   break
  fi
 done
 # Test other possible include paths
-for POSSIBLE_LIBRARY_PATH in /opt/X11/include /usr/X11R6/include /usr/include/X11 /usr/local/include/X11 ;do
+for POSSIBLE_LIBRARY_PATH in /opt/X11/include /usr/X11R6/include /usr/include/X11 /usr/local/include/X11 /opt/local/include ;do
  if [ -d "$POSSIBLE_LIBRARY_PATH" ];then
   LX11="$LX11 -I$POSSIBLE_LIBRARY_PATH"
   break
