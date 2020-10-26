c2e5b063d310767b6e5fc27cf2bb5e0e3afb821a
Kirill Sokolovsky <kirx007@gmail.com>
2020-10-26 03:10:41 +0000

    fewer digits in lightcurve_io

--- src/lightcurve_io.h
+++ src/lightcurve_io.h
@@ -23,11 +23,11 @@ static inline int write_lightcurve_point( FILE *lc_file_descriptor, double jd, d
 */
  //
  if ( NULL == comments_string ) {
-//  fprintf( lc_file_descriptor, "%.5lf %8.4lf %.4lf  %10.5lf %10.5lf %4.1lf %s\n", jd, mag, mag_err, x, y, app, string );
-  fprintf( lc_file_descriptor, "%.5lf %12.8lf %.8lf  %10.5lf %10.5lf %4.1lf %s\n", jd, mag, mag_err, x, y, app, string );
+  fprintf( lc_file_descriptor, "%.5lf %8.4lf %.4lf  %10.5lf %10.5lf %4.1lf %s\n", jd, mag, mag_err, x, y, app, string );
+//  fprintf( lc_file_descriptor, "%.5lf %12.8lf %.8lf  %10.5lf %10.5lf %4.1lf %s\n", jd, mag, mag_err, x, y, app, string );
  } else {
-//  fprintf( lc_file_descriptor, "%.5lf %8.4lf %.4lf  %10.5lf %10.5lf %4.1lf %s  %s\n", jd, mag, mag_err, x, y, app, string, comments_string );
-  fprintf( lc_file_descriptor, "%.5lf %12.8lf %.8lf  %10.5lf %10.5lf %4.1lf %s  %s\n", jd, mag, mag_err, x, y, app, string, comments_string );
+  fprintf( lc_file_descriptor, "%.5lf %8.4lf %.4lf  %10.5lf %10.5lf %4.1lf %s  %s\n", jd, mag, mag_err, x, y, app, string, comments_string );
+//  fprintf( lc_file_descriptor, "%.5lf %12.8lf %.8lf  %10.5lf %10.5lf %4.1lf %s  %s\n", jd, mag, mag_err, x, y, app, string, comments_string );
  }
  return 0;
 }
