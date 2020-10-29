28989ee9387a9bb0f69cec03dcbd58904e0a109e
Kirill Sokolovsky <kirx007@gmail.com>
2020-10-29 12:20:56 +0000

    added the internal copy of wcstools-3.9.6

---
+++ src/wcstools-3.9.6/htmlsp.c
@@ -0,0 +1,218 @@
+/* File crlf.c
+ * June 20, 2006
+ * By Jessica Mink, Harvard-Smithsonian Center for Astrophysics
+ * Send bug reports to jmink@cfa.harvard.edu
+
+   Copyright (C) 2006 
+   Smithsonian Astrophysical Observatory, Cambridge, MA USA
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+static int verbose = 0;         /* verbose/debugging flag */
+static void usage();
+static void HTMLFix();
+int StripHTMLTags();
+static char *RevMsg = "HTMLSP WCSTools 3.9.6, 31 August 2020, Jessica Mink (jmink@cfa.harvard.edu)";
+
+int
+main (ac, av)
+int ac;
+char **av;
+{
+    char *fn;
+    char *str;
+
+    /* crack arguments */
+    for (av++; --ac > 0 && *(str = *av) == '-'; av++) {
+        char c;
+        while ((c = *++str))
+        switch (c) {
+
+        case 'v':       /* more verbosity */
+            verbose++;
+            break;
+
+        default:
+            usage();
+            break;
+        }
+    }
+
+    /* There are ac remaining file names starting at av[0] */
+    if (ac == 0)
+        usage ();
+
+    while (ac-- > 0) {
+	fn = *av++;
+	if (verbose)
+    	    printf ("%s:\n", fn);
+	HTMLFix (fn);
+	if (verbose)
+	    printf ("\n");
+	}
+
+    return (0);
+}
+
+static void
+usage ()
+{
+    fprintf (stderr,"HTMLSP: Remove HTML tags from input file\n");
+    fprintf(stderr,"Usage:  htmlsp [-v] file1 file2 ... filen\n");
+    fprintf(stderr,"  -v: verbose\n");
+    exit (1);
+}
+
+static int flag;	/* 0: searching for < or &,
+			   1: searching for >,
+			   2: searching for ; after &,
+			   3: searching for </script>,</style>, --> */
+static int k=0;
+static char tempbuf[1024] = "";
+
+static void
+HTMLFix (name)
+
+char *name;
+
+{
+    char buffer[1000];
+    int fd;
+    int nbr, nnbr, i;
+
+    flag = 0;
+
+    fd = open (name, O_RDONLY);
+    nbr = 1000;
+    while (nbr > 0) {
+	nbr = read (fd, buffer, 1000);
+	if (nbr > 0) {
+	    nnbr = StripHTMLTags (buffer, nbr);
+	    (void) write (1, buffer, nnbr);
+	    }
+	}
+   return;
+}
+
+
+int
+StripHTMLTags (instring, size)
+
+char *instring;
+int size;
+
+{
+int i=0, j=0;
+char searchbuf[1024] =  "";
+
+while (i < size) {
+
+    if (flag == 0) {
+	if (instring[i] == '<') {
+	    flag = 1;
+	    tempbuf[0] = '\0';
+	    k=0;	/* track for <script>,<style>, <!-- --> etc */
+	    }
+	else if (instring[i] == '&') {
+	    flag = 2;
+	    }
+	else {
+	    instring[j] = instring[i];
+	    j++;
+	    }
+	}
+
+    else if (flag == 1) {
+	tempbuf[k] = instring[i];
+	k++;
+	tempbuf[k] = '\0';
+
+	if ((0 == strcmp(tempbuf,"script"))) {
+	    flag = 3;
+	    strcpy (searchbuf,"</script>");
+	    tempbuf[0] = '\0';
+	    k = 0;
+	    }
+	else if ((0 == strcmp(tempbuf,"style"))) {
+	    flag = 3;
+	    strcpy (searchbuf,"</style>");
+	    tempbuf[0] = '\0';
+	    k = 0;
+	    }
+	else if ((0 == strcmp(tempbuf,"!--"))) {
+	    flag = 3;
+	    strcpy (searchbuf,"-->");
+	    tempbuf[0] = '\0';
+	    k = 0;
+	    }
+
+	if (instring[i] == '>') {
+	    if ((0 == strcmp (tempbuf, "/tr"))) {
+		fprintf (stderr, "encountered end of table line\n");
+		instring[j] = (char) 10;
+		}
+	    else if ((0 == strcmp (tempbuf, "br"))) {
+		instring[j] = (char) 10;
+		}
+	    else if ((0 == strcmp (tempbuf, "p"))) {
+		instring[j] = (char) 10;
+		}
+	    else {
+		instring[j] = ' ';
+		}
+	    j++;
+	    flag = 0;
+	    }
+
+	}
+
+    else if (flag == 2) {
+	if(instring[i] == ';') {
+	    instring[j] = ' ';
+	    j++;
+	    flag = 0;
+	    }
+	}
+
+    else if(flag == 3) {
+	tempbuf[k] = instring[i];
+	k++;
+	tempbuf[k] = '\0';
+
+	if (0 == strcmp(&tempbuf[0] + k - strlen(searchbuf),searchbuf)) {
+	    flag = 0;
+	    searchbuf[0] = '\0';
+	    tempbuf[0] = '\0';
+	    k = 0;
+	    }
+	}
+
+    i++;
+    }
+
+instring[j] = '\0';
+
+return j;
+}
+
+/* Nov  5 2018	New program based on CRLF
+ */
---
+++ src/wcstools-3.9.6/isdate.c
@@ -0,0 +1,99 @@
+/* File isdate.c
+ * July 23, 2020
+ * By Jessica Mink, Harvard-Smithsonian Center for Astrophysics
+ * Send bug reports to jmink@cfa.harvard.edu
+
+   Copyright (C) 2020
+   Smithsonian Astrophysical Observatory, Cambridge, MA USA
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public License
+   as published by the Free Software Foundation; either version 2
+   of the License, or (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ *
+ * Return 1 if argument is an integer, 2 if it is floating point, else 0
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libwcs/fitshead.h"
+
+static char *RevMsg = "ISDATE WCSTools 3.9.6, 31 August 2020, Jessica Mink (jmink@cfa.harvard.edu)";
+
+int
+main (ac, av)
+int ac;
+char **av;
+{
+    char *str;
+    int arg;
+    int idate;
+    int lstr, istr;
+
+    /* Check for version or help command first */
+    arg = 0;
+    str = *(av+1);
+    if (!str || !strcmp (str, "help") || !strcmp (str, "-help")) {
+	fprintf (stderr,"%s\n",RevMsg);
+	fprintf (stderr,"Usage: isdate [-n] <string>\n");
+	fprintf (stderr,"       -n Do not return linefeed (for scripting)\n");
+	fprintf (stderr,"       Return 1 if argument is date as yyyy-mm-dd\n");
+	fprintf (stderr,"       Return 2 if argument is date as yyyy.mmdd\n");
+	fprintf (stderr,"       Return 0 otherwise\n");
+	exit (1);
+	}
+    else if (!strcmp (str, "version") || !strcmp (str, "-version")) {
+	fprintf (stderr,"%s\n",RevMsg);
+	exit (1);
+	}
+
+    /* If -n, do not print linefeed after number (for scripting) */
+    if ( !strcmp (str, "-n") ) {
+	arg = 1;
+	str = *(av+2);
+	}
+
+    /* Check to see if this is a number */
+    idate = 0;
+    lstr = strlen (str);
+    istr = isnum (str);
+    if (istr > 1) {
+	if (lstr == 10 && str[4] == '-' && str[7] == '-') {
+	    idate = 1;
+	    }
+	else if (lstr == 9 && str[4] == '.') {
+	    idate = 2;
+	    }
+	else if (lstr == 7) {
+	    if (str[4] = '-') {
+		idate = 1;
+		}
+	    else if (str[4] == '.') {
+		idate = 2;
+		}
+	    }
+	}
+    else if (istr == 1 && lstr == 4) {
+	idate = 1;
+	}
+    if (arg) {
+	printf ("%d", idate);
+	}
+    else {
+	printf ("%d\n", idate);
+	}
+
+    exit (0);
+}
+/* Jul 23 2020	New program based on ISNUM
+ */
