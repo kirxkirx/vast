a85b6db8caebf49ded3a8dac1838bfb9a9a3eb24
Kirill Sokolovsky <kirx007@gmail.com>
2021-01-08 11:13:23 +0000

    do not suggest internet connection problem if running remotely

--- lib/choose_vizier_mirror.sh
+++ lib/choose_vizier_mirror.sh
@@ -1,5 +1,37 @@
 #!/usr/bin/env bash
 
+function is_ssh_or_vnc() {
+  # Check environment variables for clues
+  command -v env &>/dev/null
+  if [ $? -eq 0 ];then
+   # vnc
+   env | grep --quiet VNCDESKTOP
+   if [ $? -eq 0 ];then
+    # Yes, we are over VNC
+    return 0
+   fi
+   # ssh
+   env | grep --quiet -e SSH_CLIENT -e SSH_CONNECTION
+   if [ $? -eq 0 ];then
+    # Yes, we are over ssh
+    return 0
+   fi
+  fi
+  ### ecursively search for sshd through the parent processes
+  ### The /proc thing obviously works only on linux
+  if [ -f /proc/$p/stat ];then
+   p=${1:-$PPID}
+   read pid name x ppid y < <( cat /proc/$p/stat )
+   # or: read pid name ppid < <(ps -o pid= -o comm= -o ppid= -p $p) 
+   [[ "$name" =~ sshd ]] && { return 0; }
+   [ "$ppid" -le 1 ]    && { return 1; }
+   is_ssh_or_vnc $ppid
+  else
+   # just assume the thing is running locally
+   return 1
+  fi # if [ -f /proc/$p/stat ];then
+}
+
 function vastrealpath {
   # On Linux, just go for the fastest option which is 'readlink -f'
   REALPATH=`readlink -f "$1" 2>/dev/null`
@@ -40,7 +72,6 @@ if [ "$LAST_CHAR_OF_VAST_PATH" != "/" ];then
 fi
 #
 
-#for vizier_mirror in vizier.u-strasbg.fr vizier.cfa.harvard.edu vizier.hia.nrc.ca vizier.nao.ac.jp data.bao.ac.cn vizier.ast.cam.ac.uk www.ukirt.jach.hawaii.edu vizier.inasan.ru ;do
 for vizier_mirror in vizier.u-strasbg.fr vizier.cfa.harvard.edu vizier.hia.nrc.ca vizier.nao.ac.jp ;do
  # 1252-0378302 is the USNO-B1.0 number of the test star
  `"$VAST_PATH"lib/find_timeout_command.sh` 30 "$VAST_PATH"lib/vizquery -site=$vizier_mirror -mime=text -out.form=mini -source=USNO-B1 -out.add=_r -sort=_r -c="HD 226868" -c.rs=2 2>/dev/null | grep --quiet "1252-0378302"
@@ -53,10 +84,18 @@ done
 
 # If we are still here - somthing is wrong
 echo "ERROR in $0
-No VizieR mirrors could be reached!
-Please check your internet connection." >> /dev/stderr
+No VizieR mirrors could be reached!" >> /dev/stderr
 
-# Print the default mirror in a faint hope that the internet connection will restore
+# Make sure we are not confusing the user with "check internet connection"
+# message if the user is connected remorely
+if [ ! -z "$PPID" ];then
+ is_ssh_or_vnc $PPID
+ if [ $? -ne 0 ];then
+  echo "Please check your internet connection." >> /dev/stderr
+ fi
+fi # if [ ! -z "$PPID" ];then
+
+# Print the default mirror in a faint hope that the connection will restore itself
 sleep 1
 echo "vizier.u-strasbg.fr"
 
