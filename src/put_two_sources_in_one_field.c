bb05cdd25e48749055def50723b154a0e59216a5
Kirill Sokolovsky <kirx007@gmail.com>
2021-07-18 10:10:00 +0000

    file input support in put_two_sources_in_one_field

--- src/put_two_sources_in_one_field.c
+++ src/put_two_sources_in_one_field.c
@@ -16,7 +16,7 @@ int format_hms_or_deg(char *coordinatestring) {
  return 0;
 }
 
-int main(int argc, char **argv) {
+int compute_angular_distance_and_print_result( char *string_RA1, char *string_Dec1, char *string_RA2, char *string_Dec2, double search_radius_arcsec ){
  double hh, mm, ss;
  double RA1_deg, DEC1_deg, RA2_deg, DEC2_deg;
 
@@ -27,80 +27,75 @@ int main(int argc, char **argv) {
  
  double cosine_value;
 
- if( argc < 5 ) {
-  fprintf(stderr, "Usage: %s RA1 DEC1 RA2 DEC2\n", argv[0]);
-  return 1;
- }
-
- if( format_hms_or_deg(argv[1]) ) {
+ if( format_hms_or_deg(string_RA1) ) {
   // Format HH:MM:SS.SS
-  sscanf(argv[1], "%lf:%lf:%lf", &hh, &mm, &ss);
+  sscanf(string_RA1, "%lf:%lf:%lf", &hh, &mm, &ss);
   hh+= mm / 60 + ss / 3600;
   hh*= 15;
   RA1_deg= hh;
  } else {
   // Format DD.DDDD
-  RA1_deg= atof(argv[1]);
+  RA1_deg= atof(string_RA1);
  }
 
- if( format_hms_or_deg(argv[2]) ) {
+ if( format_hms_or_deg(string_Dec1) ) {
   // Format DD:MM:SS.S
-  sscanf(argv[2], "%lf:%lf:%lf", &hh, &mm, &ss);
-  if( hh >= 0 && argv[2][0] != '-' )
+  sscanf(string_Dec1, "%lf:%lf:%lf", &hh, &mm, &ss);
+  if( hh >= 0 && string_Dec1[0] != '-' )
    hh+= mm / 60 + ss / 3600;
   else
    hh-= mm / 60 + ss / 3600;
   DEC1_deg= hh;
  } else {
   // Format DD.DDDD
-  DEC1_deg= atof(argv[2]);
+  DEC1_deg= atof(string_Dec1);
  }
  // Check the resulting values
  if( RA1_deg < 0.0 || RA1_deg > 360.0 ) {
-  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", argv[1], RA1_deg);
+  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", string_RA1, RA1_deg);
   return 1;
  }
  if( DEC1_deg < -90.0 || DEC1_deg > 90.0 ) {
-  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", argv[2], DEC1_deg);
+  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", string_Dec1, DEC1_deg);
   return 1;
  }
 
- if( format_hms_or_deg(argv[3]) ) {
+ if( format_hms_or_deg(string_RA2) ) {
   // Format HH:MM:SS.SS
-  sscanf(argv[3], "%lf:%lf:%lf", &hh, &mm, &ss);
+  sscanf(string_RA2, "%lf:%lf:%lf", &hh, &mm, &ss);
   hh+= mm / 60 + ss / 3600;
   hh*= 15;
   RA2_deg= hh;
  } else {
   // Format DD.DDDD
-  RA2_deg= atof(argv[3]);
+  RA2_deg= atof(string_RA2);
  }
 
- if( format_hms_or_deg(argv[4]) ) {
+ if( format_hms_or_deg(string_Dec2) ) {
   // Format DD:MM:SS.S
-  sscanf(argv[4], "%lf:%lf:%lf", &hh, &mm, &ss);
-  if( hh >= 0 && argv[4][0] != '-' )
+  sscanf(string_Dec2, "%lf:%lf:%lf", &hh, &mm, &ss);
+  if( hh >= 0 && string_Dec2[0] != '-' )
    hh+= mm / 60 + ss / 3600;
   else
    hh-= mm / 60 + ss / 3600;
   DEC2_deg= hh;
  } else {
   // Format DD.DDDD
-  DEC2_deg= atof(argv[4]);
+  DEC2_deg= atof(string_Dec2);
  }
  if( RA2_deg < 0.0 || RA2_deg > 360.0 ) {
-  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", argv[3], RA2_deg);
+  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", string_RA2, RA2_deg);
   return 1;
  }
  if( DEC2_deg < -90.0 || DEC2_deg > 90.0 ) {
-  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", argv[4], DEC2_deg);
+  fprintf(stderr, "ERROR parsing input coordinates: '%s' understood as '%lf'\n", string_Dec2, DEC2_deg);
   return 1;
  }
 
- fprintf(stderr, "%lf %lf  %lf %lf\n", RA1_deg, DEC1_deg, RA2_deg, DEC2_deg);
+ if( 0.0 == search_radius_arcsec )
+  fprintf(stderr, "%lf %lf  %lf %lf\n", RA1_deg, DEC1_deg, RA2_deg, DEC2_deg);
 
  if( MAX(RA1_deg, RA2_deg) > 180 && MIN(RA1_deg, RA2_deg) < 180 ) {
-  //  fprintf(stderr,"CIRCLE ALERT\n",in);
   if( RA1_deg > 180 )
    RA1_deg-= 360;
   if( RA2_deg > 180 )
@@ -124,11 +119,11 @@ int main(int argc, char **argv) {
   hh2+= 1;
   mm2= 0.0;
  }
- fprintf(stdout, "Average position  %02d:%02d:%05.2lf ", hh2, mm2, ss2);
+ if( 0.0 == search_radius_arcsec )
+  fprintf(stdout, "Average position  %02d:%02d:%05.2lf ", hh2, mm2, ss2);
 
  in= (DEC1_deg + DEC2_deg) / 2.0;
  hh2= (int)in;
- // fprintf(stderr,"%lf %d\n",in,hh2);
  mm2= (int)((in - hh2) * 60);
  ss2= ((in - hh2) * 60 - mm2) * 60;
  if( in < 0.0 ) {
@@ -144,9 +139,11 @@ int main(int argc, char **argv) {
   mm2= 0.0;
  }
  if( (hh2 == 0 && in < 0) || (hh2 == 0 && mm2 == 0 && in < 0) ){
-  fprintf(stdout, "-%02d:%02d:%04.1lf\n", hh2, mm2, fabs(ss2));
+  if( 0.0 == search_radius_arcsec )
+   fprintf(stdout, "-%02d:%02d:%04.1lf\n", hh2, mm2, fabs(ss2));
  } else {
-  fprintf(stdout, "%+02d:%02d:%04.1lf\n", hh2, mm2, ss2);
+  if( 0.0 == search_radius_arcsec )
+   fprintf(stdout, "%+02d:%02d:%04.1lf\n", hh2, mm2, ss2);
  }
 
  RA1_deg*= 3600.0 / ARCSEC_IN_RAD;
@@ -174,7 +171,7 @@ int main(int argc, char **argv) {
  }
  // check if the trick worked
  if( 0 != isnan(distance) ) {
-  fprintf(stderr, "ERROR in %s distance is 'nan'\n", argv[0]);
+  fprintf(stderr, "ERROR in %s distance is 'nan'\n", "compute_angular_distance_and_print_result()");
   return 1;
  }
 
@@ -191,9 +188,81 @@ int main(int argc, char **argv) {
   mm2= 0.0;
  }
 
- fprintf(stdout, "Angular distance  %02d:%02d:%05.2lf = ", hh2, mm2, ss2);
+ if( 0.0 == search_radius_arcsec ) {
+  fprintf(stdout, "Angular distance  %02d:%02d:%05.2lf = ", hh2, mm2, ss2);
+  fprintf(stdout, "%lf degrees\n", distance * ARCSEC_IN_RAD / 3600);
+ } else {
+  if( distance * ARCSEC_IN_RAD / 3600.0 < search_radius_arcsec ) {
+   return 0;
+  } else {
+   return 1;
+  }
+ }
 
- fprintf(stdout, "%lf degrees\n", distance * ARCSEC_IN_RAD / 3600);
+ return 0;
+}
+
+int main(int argc, char **argv) {
+ FILE *filelist_input_positions;
+ char str1[512];
+ char str2[512];
+ char input_buffer[4096];
+ double search_radius_arcsec;
+ unsigned int i;
+ int string_looks_ok;
+
+ if( argc < 5 ) {
+  fprintf(stderr, "Usage:\n%s RA1 DEC1 RA2 DEC2\nor\n%s RA1 DEC1 radeclist.txt search_radius_arcsec\n", argv[0], argv[0]);
+  return 1;
+ }
+
+ // check if the third argment is a file name
+ filelist_input_positions=fopen(argv[3],"r");
+ if( NULL==filelist_input_positions ) {
+  // it is not - compare just one pair of positions and exit
+  if( 0 != compute_angular_distance_and_print_result(argv[1], argv[2], argv[3], argv[4], 0.0) ){
+   return 1;
+  } else {
+   return 0;
+  }
+ }
+ // if we are still here, the third argment is a file name
+ // the file is expected to contain alist of positions
+ search_radius_arcsec=atof(argv[4]);
+ if( search_radius_arcsec<=0.0 || search_radius_arcsec>3600.0 ){
+  fprintf(stderr, "ERROR in %s -- invalid search radius in arcsec: %s\n", argv[0], argv[4]);
+  return 1;
+ }
+ //while( -1<fscanf(filelist_input_positions, "%s %s", str1, str2) ) {
+ while( NULL != fgets( input_buffer, 4096, filelist_input_positions) ) {
+  // check that the string contains white space and then something
+  input_buffer[4096-1]='\0';
+  string_looks_ok= 0;
+  for(i=1;i<strlen(input_buffer)-2;i++){
+   if( input_buffer[i]==' ' && input_buffer[i+1]!=' ' ){
+    string_looks_ok= 1;
+    break;
+   }
+  }
+  if( 0 == string_looks_ok ){
+   continue;
+  }
+  //
+  if( 2 != sscanf(input_buffer, "%s %s", str1, str2) ) {
+   str1[0]='\0';
+   str2[0]='\0';
+   continue;
+  }
+  str1[512-1]='\0';
+  str2[512-1]='\0';
+  //fprintf(stderr, "str1='%s' str2='%s'\n",str1,str2);
+  if( 0 == compute_angular_distance_and_print_result(argv[1], argv[2], str1, str2, search_radius_arcsec) ){
+   fprintf(stdout, "FOUND\n");
+   return 0;
+  }
+ }
+ fclose(filelist_input_positions);
+
 
  return 0;
 }
