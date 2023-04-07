a00bb2f60b0459b5a5b5a1376dfff46ee637d6f8
Kirill Sokolovsky <kirx007@gmail.com>
2023-04-07 20:58:40 +0000

    remove .x11test.log by prepare_vast_release.sh

--- lib/prepare_vast_release.sh
+++ lib/prepare_vast_release.sh
@@ -73,7 +73,7 @@ rm -f prelim_vast_run.log
 rm -f src/cpgplot.h src/pgfv/cpgplot.h
 #
 ####################################
-for BADFILE in vast_input_user_specified_moving_object_position.txt shuffled_lightcurve.txt test_lightcurve_outlier.txt test_lightcurve.tex test_lightcurve_without_outlier.txt ls.periodogram lk.periodogram deeming.periodogram binned_powerspectrum.periodogram out_Cepheid_TDB_HJD_VARTOOLS.tmp out_Cepheid_TT_HJD_VaST.tmp test_heliocentric_correction.tmp test_heliocentric_correction.tmp_hjdTT test_heliocentric_correction.tmp_vartools valgrind_test.out magcalibdebug.txt A 2mass.tmp script.tmp IOMC_2677000065.txt transient_factory_test31.txt testfile ;do
+for BADFILE in vast_input_user_specified_moving_object_position.txt .x11test.log shuffled_lightcurve.txt test_lightcurve_outlier.txt test_lightcurve.tex test_lightcurve_without_outlier.txt ls.periodogram lk.periodogram deeming.periodogram binned_powerspectrum.periodogram out_Cepheid_TDB_HJD_VARTOOLS.tmp out_Cepheid_TT_HJD_VaST.tmp test_heliocentric_correction.tmp test_heliocentric_correction.tmp_hjdTT test_heliocentric_correction.tmp_vartools valgrind_test.out magcalibdebug.txt A 2mass.tmp script.tmp IOMC_2677000065.txt transient_factory_test31.txt testfile ;do
  if [ -f "$BADFILE" ];then
   rm -f "$BADFILE"
  fi
