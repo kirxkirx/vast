56c5dca9c2b7f1c19402a6636e1f3a2ad61bcb39
Kirill Sokolovsky <kirx007@gmail.com>
2023-08-01 17:44:05 +0000

    do not ask for user input in update_offline_catalogs.

--- lib/update_offline_catalogs.sh
+++ lib/update_offline_catalogs.sh
@@ -257,41 +257,34 @@ if [ ! -f $TYCHO_PATH/tyc2.dat.00 ];then
  else
   #
   echo "Tycho-2 catalog was not found at $TYCHO_PATH"
-  echo "Would you like to download it now (it's big, ~160M)? (y/n)"
-  read ANSWER
-  if [ "$ANSWER" = "n" ];then
-   echo "Well, maybe next time..."
-   exit 1
-  else
-   if [ ! -d $TYCHO_PATH ];then
-    mkdir $TYCHO_PATH
-   fi
-   cd $TYCHO_PATH 
-   # remove any incomplete copy of Tycho-2
-   for i in tyc2.dat.* ;do
-    if [ -f "$i" ];then
-     rm -f "$i"
-    fi
-   done
-   #
-   #wget -nH --cut-dirs=4 --no-parent -r -l0 -c -R 'guide.*,*.gif' "ftp://cdsarc.u-strasbg.fr/pub/cats/I/259/"
-   wget -nH --cut-dirs=4 --no-parent -r -l0 -c -A 'ReadMe,*.gz,robots.txt' "http://scan.sai.msu.ru/~kirx/data/tycho2/"
-   echo "Download complete. Unpacking..."
-   for i in tyc2.dat.*gz ;do
-    # handle a very special case: `basename $i .gz` is a broken symlink
-    if [ -L `basename $i .gz` ];then
-     # if this is a symlink
-     if [ ! -e `basename $i .gz` ];then
-      # if it is broken
-      rm -f `basename $i .gz`
-      # remove that symlink
-     fi
-    fi
-    #
-    gunzip $i
-   done
-   cd $VASTDIR
+  if [ ! -d $TYCHO_PATH ];then
+   mkdir $TYCHO_PATH
   fi
+  cd $TYCHO_PATH 
+  # remove any incomplete copy of Tycho-2
+  for i in tyc2.dat.* ;do
+   if [ -f "$i" ];then
+    rm -f "$i"
+   fi
+  done
+  #
+  #wget -nH --cut-dirs=4 --no-parent -r -l0 -c -R 'guide.*,*.gif' "ftp://cdsarc.u-strasbg.fr/pub/cats/I/259/"
+  wget -nH --cut-dirs=4 --no-parent -r -l0 -c -A 'ReadMe,*.gz,robots.txt' "http://scan.sai.msu.ru/~kirx/data/tycho2/"
+  echo "Download complete. Unpacking..."
+  for i in tyc2.dat.*gz ;do
+   # handle a very special case: `basename $i .gz` is a broken symlink
+   if [ -L `basename $i .gz` ];then
+    # if this is a symlink
+    if [ ! -e `basename $i .gz` ];then
+     # if it is broken
+     rm -f `basename $i .gz`
+     # remove that symlink
+    fi
+   fi
+   #
+   gunzip $i
+  done
+  cd $VASTDIR
  fi # if [ -s ../tycho2/tyc2.dat.19 ];then 
 fi
 
