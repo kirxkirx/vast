326f64ed4f1073be7451a0d7f343f7ec080772e2
Kirill Sokolovsky <kirx007@gmail.com>
2023-08-29 00:18:49 +0000

    change the coloring convention for the transient candidates list: red now always means bad candidate

--- util/transients/MPCheck_v2.sh
+++ util/transients/MPCheck_v2.sh
@@ -24,8 +24,6 @@ if [ -z "$5" ];then
 fi
 RA=$1
 DEC=$2
-#DATE=$3
-#TIME=$4
 
 RAHH=`echo $RA |awk -F":" '{print $1}'`
 RAMM=`echo $RA |awk -F":" '{print $2}'`
@@ -38,16 +36,6 @@ DECSS=`echo $DEC |awk -F":" '{print $3}'`
 YEAR="$3"
 MONTH="$4"
 DAYFRAC="$5"
-
-#DAY=`echo $DATE |awk -F"." '{print $1}'`
-#MONTH=`echo $DATE |awk -F"." '{print $2}'`
-#YEAR=`echo $DATE |awk -F"." '{print $3}'`
-#
-#TIMEH=`echo $TIME |awk -F":" '{print $1}'`
-#TIMEM=`echo $TIME |awk -F":" '{print $2}'`
-#TIMES=`echo $TIME |awk -F":" '{print $2}'`
-#
-#DAYFRAC=`echo "$DAY $TIMEH $TIMEM $TIMES"| awk '{printf "%08.5f\n",$1+$2/24+$3/1440+$4/86400}'`
 
 if [ -z "$6" ];then
  COLOR=1
@@ -69,9 +57,23 @@ else
  DATABASE_NAME="<font color=\"teal\">astcheck</font>"
 fi
 
+#####
+# Check if the thing is (close to) a major planet
+THIS_A_PLANET=0
+EXCLUSION_LIST_FILE="planets.txt"
+if [ -s "$EXCLUSION_LIST_FILE" ];then
+ PLANET_SEARCH_RESULTS=$(lib/put_two_sources_in_one_field "$RAHH:$RAMM:$RASS" "$DECDD:$DECMM:$DECSS" "$EXCLUSION_LIST_FILE" 400)
+ echo "$PLANET_SEARCH_RESULTS" | grep --quiet "FOUND"
+ if [ $? -eq 0 ];then
+  echo "$PLANET_SEARCH_RESULTS" | awk -F'FOUND' '{print $2}'
+  THIS_A_PLANET=1
+ fi
+fi
+#####
+
+
 # This script should take care of updating astorb.dat
 lib/update_offline_catalogs.sh all
-
 
 if [ -x lib/astcheck ];then
  if [ ! -f astorb.dat ];then
@@ -101,23 +103,24 @@ if [ -x lib/astcheck ];then
  fi
 # echo "Using local copy of astcheck to identify asteroids! See http://home.gwi.net/~pluto/devel/astcheck.htm for details."
  echo "$YEAR $MONTH $DAYFRAC $RAHH $RAMM $RASS  $DECDD $DECMM $DECSS  $MAG_FOR_MPC_REPORT" |awk '{printf "     TAU0008  C%s %02.0f %08.5f %02.0f %02.0f %05.2f %+02.0f %02.0f %05.2f         %4.1f R      500\n",$1,$2,$3,$4,$5,$6,$7,$8,$9,$10}' > test.mpc
- #lib/astcheck test.mpc -r100 -m15 |grep -A 50 "TAU0008" |grep -v "TAU0008" |head -n 1 | grep -v ObsCodes.html
- # We need the 250" search radius to find ceres with the available custom (=old) astorb.dat
- #lib/astcheck test.mpc -r300 -m15 |grep -A 50 "TAU0008" |grep -v "TAU0008" |head -n 1 | grep -v ObsCodes.html
+ # 400" is the search radius
  lib/astcheck test.mpc -r400 -m15 |grep -A 50 "TAU0008" |grep -v "TAU0008" |head -n 1 | grep -v ObsCodes.html
- #lib/astcheck test.mpc -r1000 -m15 |grep -A 50 "TAU0008" |grep -v "TAU0008" |head -n 1 | grep -v ObsCodes.html
- if [ $? -eq 1 ];then
+ if [ $? -ne 0 ] && [ $THIS_A_PLANET -eq 0 ] ;then
   if [ $COLOR -eq 1 ];then
-   echo -e "The object was \033[01;31mnot found\033[00m in $DATABASE_NAME."
+   #echo -e "The object was \033[01;31mnot found\033[00m in $DATABASE_NAME."
+   echo -e "The object was \033[01;32mnot found\033[00m in $DATABASE_NAME."
   else
-   echo -e "The object was <font color=\"red\">not found</font> in $DATABASE_NAME."
+   #echo -e "The object was <font color=\"red\">not found</font> in $DATABASE_NAME."
+   echo -e "The object was <font color=\"green\">not found</font> in $DATABASE_NAME."
   fi
   exit 1 # return code will signal we have no ID
  else
   if [ $COLOR -eq 1 ];then
-   echo -e "The object was \033[01;32mfound\033[00m in $DATABASE_NAME.  "
+   #echo -e "The object was \033[01;32mfound\033[00m in $DATABASE_NAME.  "
+   echo -e "The object was \033[01;31mfound\033[00m in $DATABASE_NAME.  "
   else
-   echo -e "The object was <font color=\"green\">found</font> in $DATABASE_NAME.  "
+   #echo -e "The object was <font color=\"green\">found</font> in $DATABASE_NAME.  "
+   echo -e "<b>The object was <font color=\"red\">found</font> in $DATABASE_NAME.</b>  "
   fi 
   exit 0 # return code will signal we have an ID
  fi # else lib/astcheck 
