7ec99be2d5ab3952f14c9d2949f9125bbda2722a
Kirill Sokolovsky <kirx007@gmail.com>
2023-11-20 23:32:44 +0000

    TOCP format fix notest

--- lib/tocp_transients_list.sh
+++ lib/tocp_transients_list.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl --silent http://www.cbat.eps.harvard.edu/unconf/tocp.html | grep -e 'PNV J' -e 'TCP J' -e 'PSN J' | awk -F'>' '{print $2" "$3}' | sed -e 's/<\/a//g' -e 's/*//g' -e 's/2015 11 16 8167/2015 11 16.8167/g' -e 's/04 17 54 27/04 17 54.27/g' | awk '{print $6":"$7":"$8" "$9":"$10":"$11"  "$1" "$2"  "$3"-"$4"-"$5"  "$12"mag"}'
+curl --silent http://www.cbat.eps.harvard.edu/unconf/tocp.html | grep -e 'PNV J' -e 'TCP J' -e 'PSN J' | awk -F'>' '{print $2" "$3}' | sed -e 's/<\/a//g' -e 's/*//g' -e 's/2015 11 16 8167/2015 11 16.8167/g' -e 's/04 17 54 27/04 17 54.27/g' -e 's/01 34 21.40 +30 23 26.6./01 34 21.40 +30 23 26.6 /g' | awk '{print $6":"$7":"$8" "$9":"$10":"$11"  "$1" "$2"  "$3"-"$4"-"$5"  "$12"mag"}'
