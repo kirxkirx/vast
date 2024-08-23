6c0828f7e9b2596833f186d0e7c2acbdaf02b8a9
Kirill Sokolovsky <kirx007@gmail.com>
2024-08-23 05:47:14 +0000

    simplify draw_stars_with_ds9.sh

--- util/draw_stars_with_ds9.sh
+++ util/draw_stars_with_ds9.sh
@@ -25,7 +25,9 @@ else
  WCS_OPERATION_MODE=0
 fi
 
-echo "SCRIPT_NAME= $SCRIPT_NAME   WCS_OPERATION_MODE= $WCS_OPERATION_MODE"
+RANDOM_TMPFILE_SUFFIX=truncated_string="${$}${USER:0:8}"
+
+echo "SCRIPT_NAME= $SCRIPT_NAME   WCS_OPERATION_MODE= $WCS_OPERATION_MODE   RANDOM_TMPFILE_SUFFIX= $RANDOM_TMPFILE_SUFFIX"
 
 
 echo "Working..."
@@ -43,7 +45,7 @@ if [ -n "$3" ];then
   if [ ! -z "$4" ];then
    AP=`echo "$4/2"|bc -ql`
    #FILENAME=$5
-   #head -n 1 $FILENAME > /tmp/read"$$""$USER".tmp
+   #head -n 1 $FILENAME > /tmp/read"$RANDOM_TMPFILE_SUFFIX".tmp
    #FILENAME=`basename $FILENAME .dat`
    if [ ! -z "$5" ];then
     StringW=$(echo $5 | awk -F'out' '{print $2}' | awk -F'.dat' '{print $1}') 
@@ -62,8 +64,8 @@ if [ -n "$3" ];then
   AP=0.005
   StringW="MARKED_POSITION"
   echo "Marking WCS position:  $2 $3  ($X, $Y)"
-  echo "point($X,$Y) # point=x" >> /tmp/reg"$$""$USER".reg
-  #echo "circle($X,$Y,$AP)" >> /tmp/reg"$$""$USER".reg
+  echo "point($X,$Y) # point=x" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
+  #echo "circle($X,$Y,$AP)" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
   if [ -f vast_star_catalog.log ] ;then
    echo "vast_star_catalog.log found!"
    BEST_DIST=1.0
@@ -88,10 +90,10 @@ if [ -n "$3" ];then
     CLOSEST_STAR_NUMBER=`echo "$STRING"|awk '{print $3}'`
     X2=`lib/hms2deg $BEST_RA $BEST_DEC |head -n1`
     Y2=`lib/hms2deg $BEST_RA $BEST_DEC |tail -n1 |awk '{print $1+0.0}'` # get rid of '+' sign if it's there
-    echo "circle($X2,$Y2,$AP)" >> /tmp/reg"$$""$USER".reg
+    echo "circle($X2,$Y2,$AP)" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
     X2=`echo "$X2 $AP" | awk '{print $1+2.0*$2}'`
     Y2=`echo "$Y2 $AP" | awk '{print $1+2.0*$2}'`
-    echo "# text($X2,$Y2) text={$CLOSEST_STAR_NUMBER}" >> /tmp/reg"$$""$USER".reg
+    echo "# text($X2,$Y2) text={$CLOSEST_STAR_NUMBER}" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
    fi
   fi
  fi
@@ -100,7 +102,7 @@ if [ -n "$3" ];then
   Y=`echo "$Y $AP" | awk '{print $1+2.0*$2}'` 
  fi
  echo "# text($X,$Y) text={$StringW}
-circle($X,$Y,$AP)" >> /tmp/reg"$$""$USER".reg
+circle($X,$Y,$AP)" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
  
 
 else
@@ -137,22 +139,26 @@ else
   echo "ERROR in $0 : cannot display reference image $REFERENCE_IMAGE"
   exit 1
  fi
+ if [ ! -s "vast_image_details.log" ];then
+  echo "ERROR in $0 : cannot read the log file 'vast_image_details.log'"
+  exit 1
+ fi
+ #AP=$(grep "$REFERENCE_IMAGE" vast_image_details.log | head -n1 | awk '{print $9}')
  echo "Reading pixel positions of stars from 'vast_lightcurve_statistics.log' (accepting only the ones that were detected at the reference image)..."
  # read the file vast_lightcurve_statistics.log
  TOTAL_STAR_COUNTER=0
  GOOD_STAR_COUNTER=0
  while read TMP TMP2 TMP3 TMP4 FILENAME REST ;do
-  head -n 1 $FILENAME > /tmp/read"$$""$USER".tmp
-  read JD MAG MERR X Y AP FITSFILE REST < /tmp/read"$$""$USER".tmp
+  #head -n 1 $FILENAME > /tmp/read"$RANDOM_TMPFILE_SUFFIX".tmp
+  #read JD MAG MERR X Y AP FITSFILE REST < /tmp/read"$RANDOM_TMPFILE_SUFFIX".tmp
+  # The <<< operator was introduced in Bash version 2.05b, released in 2002.
+  read JD MAG MERR X Y AP FITSFILE REST <<< $(head -n 1 $FILENAME)
   # Check if this star was actually detected on the reference image or not
-  #grep "Ref.  image:" vast_summary.log > /tmp/read"$$""$USER".grp
-  #read A B C D E F OTHERCOLUMNSJUSTINCASE < /tmp/read"$$""$USER".grp
-  #rm -f /tmp/read"$$""$USER".grp
   if [ "$REFERENCE_IMAGE" = "$FITSFILE" ];then
   
    #AP=`echo "$AP/2"|bc -ql`
    AP=`echo "$AP" | awk '{print $1/2}'`
-   echo "circle($X,$Y,$AP)" >> /tmp/reg"$$""$USER".reg
+   echo "circle($X,$Y,$AP)" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
    Y=`echo "$Y $AP" | awk '{print $1+1.8*$2}'`
    FILENAME=`basename $FILENAME .dat`
  
@@ -163,7 +169,7 @@ else
       echo "or"
       echo "$0 image.fit X_star Y_star Ap_size outNNNN.dat"
       echo " -l near each star write it's number and its instrumental magnitude"
-      echo " -s near each star write it's number only"
+      echo " -s near each star write it's number only (default)"
       echo " -h print this message"
       exit 1;;
       -l | --l )
@@ -172,18 +178,18 @@ else
      -s | --s | * )
     StringW=`echo $FILENAME | awk -F out '{print $2}'` ;;
    esac  
-   echo "# text($X,$Y) text={$StringW}" >> /tmp/reg"$$""$USER".reg
+   echo "# text($X,$Y) text={$StringW}" >> /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg
    GOOD_STAR_COUNTER=$[$GOOD_STAR_COUNTER + 1 ]
   fi
   TOTAL_STAR_COUNTER=$[$TOTAL_STAR_COUNTER + 1 ]
  done < vast_lightcurve_statistics.log
- echo "Processed $TOTAL_STAR_COUNTER stars, $GOOD_STAR_COUNTER of them were detected on the reference image (so they are added to the DS9 region file for display)"
+ echo "Processed $TOTAL_STAR_COUNTER stars listed in 'vast_lightcurve_statistics.log', $GOOD_STAR_COUNTER of them were detected on the reference image (so they are added to the DS9 region file for display)"
  if [ $GOOD_STAR_COUNTER -eq 0 ];then
   echo "ERROR: no good stars written to the region file"
   exit 1
  fi
- if [ ! -s /tmp/reg"$$""$USER".reg ];then
-  echo "ERROR: cannot read the region file /tmp/reg"$$""$USER".reg"
+ if [ ! -s /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg ];then
+  echo "ERROR: cannot read the region file /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg"
   exit 1
  fi
  # Make sure that we'll display the reference image, not the last one
@@ -198,20 +204,24 @@ if [ -f $WCSFITSFILE ];then
 fi
 
 # Prepare a header for DS9 region file 
-echo "Writing DS9 region file " /tmp/reg2"$$""$USER".reg
-echo "# Region file format: DS9 version 4.0" > /tmp/reg2"$$""$USER".reg
-echo "# Filename: $FITSFILE" >> /tmp/reg2"$$""$USER".reg
-echo "global color=green font=\"sans 10 normal\" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source" >> /tmp/reg2"$$""$USER".reg
+echo "Writing DS9 region file " /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
+echo "# Region file format: DS9 version 4.0" > /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
+echo "# Filename: $FITSFILE" >> /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
+echo "global color=green font=\"sans 10 normal\" select=1 highlite=1 edit=1 move=1 delete=1 include=1 fixed=0 source" >> /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
 if [ $WCS_OPERATION_MODE -eq 0 ];then
- echo "image" >> /tmp/reg2"$$""$USER".reg
+ echo "image" >> /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
 else
- echo "fk5" >> /tmp/reg2"$$""$USER".reg
+ echo "fk5" >> /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
 fi
-cat /tmp/reg"$$""$USER".reg >> /tmp/reg2"$$""$USER".reg
-rm -f /tmp/reg"$$""$USER".reg /tmp/read"$$""$USER".tmp
-echo "#### head of the DS9 region file /tmp/reg2"$$""$USER".reg ####"
+cat /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg >> /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
+for FILE_TO_BE_REMOVED in /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg /tmp/read"$RANDOM_TMPFILE_SUFFIX".tmp ;do
+ if [ -f "$FILE_TO_BE_REMOVED" ];then
+  rm -f "$FILE_TO_BE_REMOVED"
+ fi
+done
+echo "#### head of the DS9 region file /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg ####"
 echo "####################################################"
-head /tmp/reg2"$$""$USER".reg
+head /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
 # Print " ..... " if the region file is long (as expected)
 if [ -n "$GOOD_STAR_COUNTER" ];then
  if [ $GOOD_STAR_COUNTER -gt 3 ];then
@@ -219,8 +229,8 @@ if [ -n "$GOOD_STAR_COUNTER" ];then
  fi
 fi
 echo "####################################################"
-echo "Starting DS9 on image $FITSFILE with region file" /tmp/reg2"$$""$USER".reg
-ds9 $FITSFILE -region /tmp/reg2"$$""$USER".reg -xpa no 
+echo "Starting DS9 on image $FITSFILE with region file" /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg
+ds9 $FITSFILE -region /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg -xpa no 
 echo "Removing temporary files"
-rm -fv /tmp/reg2"$$""$USER".reg /tmp/reg"$$""$USER".reg /tmp/read"$$""$USER".tmp
+rm -fv /tmp/reg2"$RANDOM_TMPFILE_SUFFIX".reg /tmp/reg"$RANDOM_TMPFILE_SUFFIX".reg /tmp/read"$RANDOM_TMPFILE_SUFFIX".tmp
 echo "All done =)"
