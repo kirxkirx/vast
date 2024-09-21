0811c373b0eb30acbfc3b407674cdb43bdd017aa
Kirill Sokolovsky <kirx@kirx.net>
2024-09-21 10:08:50 +0000

    start update_offline_catalogs.sh from any dir

--- lib/update_offline_catalogs.sh
+++ lib/update_offline_catalogs.sh
@@ -50,7 +50,54 @@ function vastrealpath() {
   fi # readlink -f
   echo "$REALPATH"
 }
-VASTDIR=$(vastrealpath $PWD)
+
+# Function to remove the last occurrence of a directory from a path
+remove_last_occurrence() {                                         
+    echo "$1" | awk -F/ -v dir=$2 '{
+        found = 0;
+        for (i=NF; i>0; i--) {
+            if ($i == dir && found == 0) {
+                found = 1;
+                continue;
+            }
+            res = (i==NF ? $i : $i "/" res);
+        }
+        print res;
+    }'
+}
+
+# Function to get full path to vast main directory from the script name
+get_vast_path_ends_with_slash_from_this_script_name() {
+ VAST_PATH=$(vastrealpath $0)
+ VAST_PATH=$(dirname "$VAST_PATH")
+
+ # Remove last occurrences of util, lib, examples
+ VAST_PATH=$(remove_last_occurrence "$VAST_PATH" "util")
+ VAST_PATH=$(remove_last_occurrence "$VAST_PATH" "lib")
+ VAST_PATH=$(remove_last_occurrence "$VAST_PATH" "examples")
+ VAST_PATH=$(remove_last_occurrence "$VAST_PATH" "transients")
+
+ # Make sure no '//' are left in the path (they look ugly)
+ VAST_PATH="${VAST_PATH/'//'/'/'}"
+ # In case the above line didn't work
+ VAST_PATH=$(echo "$VAST_PATH" | sed "s:/'/:/:g")
+
+ # Make sure no quotation marks are left in VAST_PATH
+ VAST_PATH=$(echo "$VAST_PATH" | sed "s:'::g")
+
+ # Check that VAST_PATH ends with '/'
+ LAST_CHAR_OF_VAST_PATH="${VAST_PATH: -1}"
+ if [ "$LAST_CHAR_OF_VAST_PATH" != "/" ];then
+  VAST_PATH="$VAST_PATH/"
+ fi
+
+ echo "$VAST_PATH"
+}
+
+
+VASTDIR=$(get_vast_path_ends_with_slash_from_this_script_name "$0")
+
+cd "$VASTDIR" || exit 1
 
 # By default, do not download VSX and astorb.dat if they were not downloaded earlier
 DOWNLOAD_EVERYTHING=0
