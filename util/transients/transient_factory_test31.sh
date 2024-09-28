fef493445eb722de8e6c9ff3d69cc6efd79b37e3
Kirill Sokolovsky <kirx@kirx.net>
2024-09-28 10:23:17 +0000

    further support for MPC codes

--- util/transients/transient_factory_test31.sh
+++ util/transients/transient_factory_test31.sh
@@ -112,7 +112,7 @@ if [ -n "$CAMERA_SETTINGS" ];then
   TELESCOP_NAME_KNOWN_TO_VaST_FOR_FOV_DETERMINATION="NMW_camera"
   BAD_REGION_FILE="../Stas_bad_region.lst"
   EXCLUSION_LIST="../exclusion_list.txt"
-  MPC_CODE=C32
+  export MPC_CODE=C32
   # Calibration data
   if [ -z "$DARK_FRAMES_DIR" ];then
    export DARK_FRAMES_DIR=/dataX/cgi-bin/unmw/uploads/darks
@@ -128,7 +128,7 @@ if [ -n "$CAMERA_SETTINGS" ];then
   # Canon 135 mm f/2.0 telephoto lens + SBIG STL-11000 CCD, 20 sec exposures
   echo "### Using search settings for $CAMERA_SETTINGS camera ###"
   export AAVSO_COMMENT_STRING="NMW Camera-2 Canon 135mm f/2.0 telephoto lens + SBIG STL-11000M CCD"
-  MPC_CODE=C32
+  export MPC_CODE=C32
   # The input images will be calibrated
   # DARK_FRAMES_DIR has to be pointed at directory containing dark frames,
   # the script will try to find the most appropriate one based on temperature and time
@@ -168,7 +168,7 @@ if [ -n "$CAMERA_SETTINGS" ];then
  if [ "$CAMERA_SETTINGS" = "TICA_TESS_FFI" ];then
   # TICA TESS FFIs downloaded from https://archive.stsci.edu/hlsp/tica#section-c34b9669-b0be-40b2-853e-a59997d1b7c5
   echo "### Using search settings for $CAMERA_SETTINGS camera ###"
-  MPC_CODE="500@-95"
+  export MPC_CODE="500@-95"
   NUMBER_OF_DETECTED_TRANSIENTS_BEFORE_FILTERING_SOFT_LIMIT=1000
   NUMBER_OF_DETECTED_TRANSIENTS_BEFORE_FILTERING_HARD_LIMIT=1500
   export FILTER_FAINT_MAG_CUTOFF_TRANSIENT_SEARCH="15.0"
