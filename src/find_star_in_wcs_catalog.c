dd4357ac6b91d1a4313a7cc7b6709f6f543536c1
Kirill Sokolovsky <kirx007@gmail.com>
2024-12-18 20:44:43 +0000

    correct negative RA in find_star_in_wcs_catalog

--- src/find_star_in_wcs_catalog.c
+++ src/find_star_in_wcs_catalog.c
@@ -13,17 +13,16 @@ int main( int argc, char **argv ) {
  double distance;
  double best_distance= 10; // 10 pix
  int best_i= -1;
- double target_RA_deg;
- double target_Dec_deg;
- // char GARBAGE[4096];
+ double input_target_x_pix;
+ double input_target_y_pix;
  char sextractor_catalog_string[MAX_STRING_LENGTH_IN_SEXTARCTOR_CAT];
 
  if ( argc < 3 ) {
-  fprintf( stderr, "Usage: %s target_RA_deg target_Dec_deg\n", argv[0] );
+  fprintf( stderr, "Usage: %s input_target_x_pix input_target_y_pix\n", argv[0] );
  }
 
- target_RA_deg= atof( argv[1] );
- target_Dec_deg= atof( argv[2] );
+ input_target_x_pix= atof( argv[1] );
+ input_target_y_pix= atof( argv[2] );
  NUMBER= malloc( MAX_NUMBER_OF_STARS * sizeof( int ) );
  if ( NUMBER == NULL ) {
   fprintf( stderr, "ERROR: Couldn't allocate memory for NUMBER(find_star_in_wcs_catalog.c)\n" );
@@ -50,28 +49,54 @@ int main( int argc, char **argv ) {
   exit( EXIT_FAILURE );
  };
 
- /*
- while ( -1 < fscanf( stdin, "%d %lf %lf %lf %lf  %[^\t\n]", &NUMBER[i], &ALPHA_SKY[i], &DELTA_SKY[i], &X_IMAGE[i], &Y_IMAGE[i], GARBAGE ) ) {
-  i++;
- }
- for ( j= 0; j < i; j++ ) {
-  distance= sqrt( ( target_RA_deg - X_IMAGE[j] ) * ( target_RA_deg - X_IMAGE[j] ) + ( target_Dec_deg - Y_IMAGE[j] ) * ( target_Dec_deg - Y_IMAGE[j] ) );
-  if ( distance < best_distance ) {
-   best_distance= distance;
-   best_i= j;
-  }
- }
-*/
  // this might be more stable against corrupted input
  i= 0;
+ //while ( NULL != fgets( sextractor_catalog_string, MAX_STRING_LENGTH_IN_SEXTARCTOR_CAT, stdin ) ) {
+ // if ( 5 != sscanf( sextractor_catalog_string, "%d %lf %lf %lf %lf ", &NUMBER[i], &ALPHA_SKY[i], &DELTA_SKY[i], &X_IMAGE[i], &Y_IMAGE[i] ) ) {
+ //  continue;
+ // }
+ // i++;
+ //}
+ //
  while ( NULL != fgets( sextractor_catalog_string, MAX_STRING_LENGTH_IN_SEXTARCTOR_CAT, stdin ) ) {
-  if ( 5 != sscanf( sextractor_catalog_string, "%d %lf %lf %lf %lf ", &NUMBER[i], &ALPHA_SKY[i], &DELTA_SKY[i], &X_IMAGE[i], &Y_IMAGE[i] ) ) {
+  if ( 5 != sscanf( sextractor_catalog_string, "%d %lf %lf %lf %lf",
+                    &NUMBER[i], &ALPHA_SKY[i], &DELTA_SKY[i], &X_IMAGE[i], &Y_IMAGE[i] ) ) {
    continue;
   }
+
+  // Check NUMBER[i]
+  if ( NUMBER[i] < 0 ) {
+   continue;
+  }
+
+  // ALPHA_SKY[i] should be between -360 and 720 
+  if ( ALPHA_SKY[i] < -360.0 || ALPHA_SKY[i] > 720.0 ) {
+   continue;
+  }
+
+  // Adjust ALPHA_SKY[i] if needed 
+  if ( ALPHA_SKY[i] < 0.0 ) {
+   ALPHA_SKY[i] += 360.0;
+  } else if ( ALPHA_SKY[i] >= 360.0 ) {
+   ALPHA_SKY[i] -= 360.0;
+  }
+
+  // DELTA_SKY[i] should be between -90 and 90 
+  if ( DELTA_SKY[i] < -90.0 || DELTA_SKY[i] > 90.0 ) {
+   continue;
+  }
+
+  // X_IMAGE[i] and Y_IMAGE[i] should be between -MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK and MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK 
+  if ( X_IMAGE[i] < -1.0 * MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK || X_IMAGE[i] > MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK ||
+       Y_IMAGE[i] < -1.0 * MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK || Y_IMAGE[i] > MAX_IMAGE_SIDE_PIX_FOR_SANITY_CHECK ) {
+   continue;
+  }
+
   i++;
  }
+ //
  for ( j= 0; j < i; j++ ) {
-  distance= sqrt( ( target_RA_deg - X_IMAGE[j] ) * ( target_RA_deg - X_IMAGE[j] ) + ( target_Dec_deg - Y_IMAGE[j] ) * ( target_Dec_deg - Y_IMAGE[j] ) );
+  distance= sqrt( ( input_target_x_pix - X_IMAGE[j] ) * ( input_target_x_pix - X_IMAGE[j] ) + ( input_target_y_pix - Y_IMAGE[j] ) * ( input_target_y_pix - Y_IMAGE[j] ) );
   if ( distance < best_distance ) {
    best_distance= distance;
    best_i= j;
@@ -90,7 +115,7 @@ int main( int argc, char **argv ) {
 
  // Report error if we didn't find the star
  if ( best_i == -1 ) {
-  fprintf( stderr, "ERROR: cannot find a star near the specified position x=%lf y=%lf\n", target_RA_deg, target_Dec_deg );
+  fprintf( stderr, "ERROR: cannot find a star near the specified position x=%lf y=%lf\n", input_target_x_pix, input_target_y_pix );
   return 1;
  }
 
