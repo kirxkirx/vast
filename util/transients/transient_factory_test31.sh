a9a0ae893af4b0c6b127bdb9fc00bbae2c578705
Kirill Sokolovsky <kirx@kirx.net>
2026-04-15 18:46:00 -0500

    update TTUQ2b1x1 flat

--- util/transients/transient_factory_test31.sh
+++ util/transients/transient_factory_test31.sh
@@ -356,7 +356,8 @@ if [ -n "$CAMERA_SETTINGS" ];then
   # FLAT_FIELD_FILE has to point to one specific file that will be used for flat-fielding
   if [ -z "$FLAT_FIELD_FILE" ];then
    #export FLAT_FIELD_FILE="$NMW_CALIBRATION/$CAMERA_SETTINGS/flats/mff_TTUQ2b1x1_2026-02-21.fit"
-   export FLAT_FIELD_FILE="$NMW_CALIBRATION/$CAMERA_SETTINGS/flats/mff_TTUQ2b1x1_2026-03-20.fit"
+   #export FLAT_FIELD_FILE="$NMW_CALIBRATION/$CAMERA_SETTINGS/flats/mff_TTUQ2b1x1_2026-03-20.fit"
+   export FLAT_FIELD_FILE="$NMW_CALIBRATION/$CAMERA_SETTINGS/flats/mff_TTUQ2b1x1_2026-04-08_multinight.fit"
   fi
   #
   #TELESCOP_NAME_KNOWN_TO_VaST_FOR_FOV_DETERMINATION="STL-11000M"
